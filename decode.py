ebffa541101f89b273485121ccb0476682ffbe74
jordanxlau <jordanxlau@yahoo.ca>
2025-03-24 22:49:47 -0400

    bugfix: model properly loaded and saved

--- decode.py
+++ decode.py
@@ -2,18 +2,21 @@ import random
 
 from data import af_vectorization, eng_vectorization, test_pairs
 from keras import ops, models
-from utils import PositionalEmbedding, TransformerDecoder, TransformerEncoder
-import sys
+
+from utils import PositionalEmbedding, TransformerEncoder, TransformerDecoder
+
 # Get saved model
-transformer = models.load_model("af-en-v1.keras")
+transformer = models.load_model("af-en-v1.keras", custom_objects={
+    'PositionalEmbedding': PositionalEmbedding,
+    'TransformerEncoder': TransformerEncoder,
+    'TransformerDecoder': TransformerDecoder
+})
 
 # Decoding test sentences
 af_vocab = af_vectorization.get_vocabulary()
 af_index_lookup = dict(zip(range(len(af_vocab)), af_vocab))
 max_decoded_sentence_length = 20
 
-print("BOOGAWOOGA")
-sys.exit()
 def decode_sequence(input_sentence):
     tokenized_input_sentence = eng_vectorization([input_sentence])
     decoded_sentence = "[start]"
@@ -42,3 +45,4 @@ test_eng_texts = [pair[0] for pair in test_pairs]
 for _ in range(30):
     input_sentence = random.choice(test_eng_texts)
     translated = decode_sequence(input_sentence)
+    print(input_sentence, translated)
