b82b9af4b2faa5a1faa474c5217fb8e2d1350941
jordanxlau <jordanxlau@yahoo.ca>
2025-03-25 21:42:06 -0400

    bugfix: generalized to lang1 and lang2 in decode.py script

--- decode.py
+++ decode.py
@@ -1,27 +1,27 @@
 import random
 
-from data import af_vectorization, eng_vectorization, test_pairs
+from data import lang1_vectorization, lang2_vectorization, test_pairs, lang1, lang2
 from keras import ops, models
 
 from utils import PositionalEmbedding, TransformerEncoder, TransformerDecoder
 
 # Get saved model
-transformer = models.load_model("af-en-v1.keras", custom_objects={
+transformer = models.load_model(lang1+"-"+lang2+"-v1.keras", custom_objects={
     'PositionalEmbedding': PositionalEmbedding,
     'TransformerEncoder': TransformerEncoder,
     'TransformerDecoder': TransformerDecoder
 })
 
 # Decoding test sentences
-af_vocab = af_vectorization.get_vocabulary()
-af_index_lookup = dict(zip(range(len(af_vocab)), af_vocab))
+lang2_vocab = lang2_vectorization.get_vocabulary()
+lang2_index_lookup = dict(zip(range(len(lang2_vocab)), lang2_vocab))
 max_decoded_sentence_length = 20
 
 def decode_sequence(input_sentence):
-    tokenized_input_sentence = eng_vectorization([input_sentence])
+    tokenized_input_sentence = lang1_vectorization([input_sentence])
     decoded_sentence = "[start]"
     for i in range(max_decoded_sentence_length):
-        tokenized_target_sentence = af_vectorization([decoded_sentence])[:, :-1]
+        tokenized_target_sentence = lang2_vectorization([decoded_sentence])[:, :-1]
         predictions = transformer(
             {
                 "encoder_inputs": tokenized_input_sentence,
@@ -33,7 +33,7 @@ def decode_sequence(input_sentence):
         sampled_token_index = ops.convert_to_numpy(
             ops.argmax(predictions[0, i, :])
         ).item(0)
-        sampled_token = af_index_lookup[sampled_token_index]
+        sampled_token = lang2_index_lookup[sampled_token_index]
         decoded_sentence += " " + sampled_token
 
         if sampled_token == "[end]":
