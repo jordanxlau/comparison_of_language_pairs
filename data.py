8c7e8250a1997c644a70e982f9a12761996fdfb5
jordanxlau <jordanxlau@yahoo.ca>
2025-03-31 01:17:58 -0400

    fix: typo edits in data.py

--- data.py
+++ data.py
@@ -11,13 +11,13 @@ from tensorflow import strings as tf_strings
 # Set a random seed, for reproducibility
 random.seed(58)
 
-lang1 = 'en' #arabic
-lang2 = 'nl' #english
+lang1 = 'en'
+lang2 = 'nl'
 
 # Downloading the data
 text = pd.read_parquet("hf://datasets/Helsinki-NLP/opus-100/"+lang1+"-"+lang2+"/train-00000-of-00001.parquet")
 
-# Limit to 200 000 examples
+# Limit to 500 000 examples
 text = text.head(500000)
 
 # Parsing the data
